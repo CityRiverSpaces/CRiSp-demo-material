4c11994c02b22fa80ce534a02cfc73a2e2b6c2a7
Francesco Nattino <f.nattino@esciencecenter.nl>
2025-03-24 10:55:23 +0100

    move common functions to utils

--- scripts/generate-corridor.R
+++ scripts/generate-corridor.R
@@ -1,3 +1,6 @@
+source("scripts/utils.R")
+
+
 cr <- tibble::tribble(
   ~city_name, ~river_name,
   "Bucharest", "Dâmbovița",
@@ -17,41 +20,6 @@ cr <- tibble::tribble(
   # "Los Angeles", "Los Angeles River",  # AOI not split
   # "Ljubljana", "Ljubljanica",  # AOI not split
 )
-
-
-#' Retry function call
-#'
-#' Functions running API calls sometimes fail and need to be retried. This
-#' wrapper function re-runs such functions a given number of times with a delay
-#' in-between calls.
-#'
-#' @param func  Function to re-run
-#' @param ...  Function parameters
-#' @param max_retries  Maximum number of retries
-#' @param delay  Delay between retries in seconds
-#'
-#' @return Value returned by wrapped function
-retry <- function(func, ..., max_retries = 5, delay = 2) {
-  attempt <- 1
-  while (attempt <= max_retries) {
-    result <- tryCatch({
-      func(...)  # Call the function with arguments
-    }, error = function(e) {
-      message(sprintf("Attempt %d failed: %s", attempt, e$message))
-      return(NULL)
-    })
-
-    if (!is.null(result)) {
-      return(result)  # Successfully retrieved result
-    }
-
-    message(sprintf("Retrying in %d seconds...", delay))
-    Sys.sleep(delay)
-    attempt <- attempt + 1
-  }
-
-  stop("Function failed after multiple attempts.")
-}
 
 
 get_river_centerline <- function(bbox, river_name, crs,
@@ -90,23 +58,6 @@ get_river_surface <- function(bbox, river_centerline, crs,
     sf::st_transform(crs) |>
     sf::st_filter(river_centerline, .predicate = sf::st_intersects) |>
     sf::st_union()
-}
-
-
-get_segments <- function(corridor, network, river_centerline) {
-  corridor_buffer <- sf::st_buffer(corridor, 100)
-  network_filtered <- filter_network(network, corridor_buffer)
-  CRiSp::delineate_segments(corridor, network_filtered, river_centerline)
-}
-
-
-filter_network <- function(network, target) {
-  network |>
-    tidygraph::activate("nodes") |>
-    tidygraph::filter(sfnetworks::node_intersects(target)) |>
-    # keep only the main connected component of the network
-    tidygraph::activate("nodes") |>
-    dplyr::filter(tidygraph::group_components() == 1)
 }
 
 
--- scripts/generate-figures-NAC-poster.R
+++ scripts/generate-figures-NAC-poster.R
@@ -1,49 +1,12 @@
+source("scripts/utils.R")
+
+
 city_name <- "Bucharest"
 river_name <- "Dâmbovița"
 network_buffer <- 3500
 dem_buffer <- 3500
 buildings_buffer <- 200
 
-#' Retry function call
-#'
-#' Functions running API calls sometimes fail and need to be retried. This
-#' wrapper function re-runs such functions a given number of times with a delay
-#' in-between calls.
-#'
-#' @param func  Function to re-run
-#' @param ...  Function parameters
-#' @param max_retries  Maximum number of retries
-#' @param delay  Delay between retries in seconds
-#'
-#' @return Value returned by wrapped function
-retry <- function(func, ..., max_retries = 5, delay = 2) {
-  attempt <- 1
-  while (attempt <= max_retries) {
-    result <- tryCatch({
-      func(...)  # Call the function with arguments
-    }, error = function(e) {
-      message(sprintf("Attempt %d failed: %s", attempt, e$message))
-      return(NULL)
-    })
-
-    if (!is.null(result)) {
-      return(result)  # Successfully retrieved result
-    }
-
-    message(sprintf("Retrying in %d seconds...", delay))
-    Sys.sleep(delay)
-    attempt <- attempt + 1
-  }
-
-  stop("Function failed after multiple attempts.")
-}
-
-
-get_segments <- function(corridor, network, river_centerline) {
-  corridor_buffer <- sf::st_buffer(corridor, 100)
-  network_filtered <- CRiSp:::filter_network(network, corridor_buffer)
-  CRiSp::delineate_segments(corridor, network_filtered, river_centerline)
-}
 
 delineate <- function(city_name, river_name) {
   # Download datasets and setup
@@ -90,6 +53,7 @@ delineate <- function(city_name, river_name) {
     riverspace = riverspace
   )
 }
+
 
 make_fig <- function(x, city_name, river_name, type,
                      out_dir = file.path(".", "figures", "poster")) {
@@ -141,6 +105,7 @@ make_fig <- function(x, city_name, river_name, type,
   # Close figure
   dev.off()
 }
+
 
 delineation <- delineate(city_name, river_name)
 make_fig(delineation, city_name, river_name, "valley")
