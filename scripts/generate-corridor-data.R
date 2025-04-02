3986c78f2e4e9e12ec4dbb652517066c79c24ee0
SarahAlidoost <f.alidoost@esciencecenter.nl>
2025-04-02 12:00:43 +0200

    fix the scripts, refactor, add new cities to the table

--- scripts/generate-corridor-data.R
+++ scripts/generate-corridor-data.R
@@ -1,29 +1,41 @@
 source("scripts/utils.R")
 library(CRiSp)
 
+# nolint start
 city_river_names <- tibble::tribble(
   ~city_name,       ~river_name,          ~network_buffer, ~buildings_buffer, ~dem_buffer,
   "Bucharest",      "Dâmbovița",           2500,             100,              2500,
-  # "Bucharest",      "Colentina",
-  # "Iaşi",           "Bahlui",
-  # "Köln",           "Rhein",
-  # "Warsaw",         "Wisła",
-  # "Arnhem",         "Nederrijn",
-  # "Graz",           "Mur",
-  # "Prague",         "Vltava",
-  # "Roma",           "Tevere",
-  # "Cairo, Egypt",   "Nile River",
-  # "Madrid",         "Río Manzanares",
-  # "Turin",          "Fiume Po",
-  # "Sheffield, UK",  "River Don",
-  # "Miercurea Ciuc", "Olt",
-  # "Los Angeles",    "Los Angeles River",
-  # "Ljubljana",      "Ljubljanica",
-  # "Paris",          "La Seine",
-  # "Berlin",         "Spree",
-)
+  "Bucharest",      "Colentina",           2500,             100,              2500,
+  "Iaşi",           "Bahlui",              2500,             100,              2500,
+  "Köln",           "Rhein",               2500,             100,              2500,
+  "Warsaw",         "Wisła",               2500,             100,              2500,
+  "Arnhem",         "Nederrijn",           2500,             100,              2500,
+  "Graz",           "Mur",                 2500,             100,              2500,
+  "Prague",         "Vltava",              2500,             100,              2500,
+  "Roma",           "Tevere",              3500,             100,              3500,
+  "Cairo, Egypt",   "Nile River",          2500,             100,              2500,
+  "Madrid",         "Río Manzanares",      2500,             100,              2500,
+  "Turin",          "Fiume Po",            2500,             100,              2500,
+  "Sheffield, UK",  "River Don",           2500,             100,              2500,
+  "Miercurea Ciuc", "Olt",                 2500,             100,              2500,
+  "Los Angeles",    "Los Angeles River",   2500,             100,              2500,
+  "Ljubljana",      "Ljubljanica",         2500,             100,              2500,
+  "Paris",          "La Seine",            2500,             100,              2500,
+  "Berlin",         "Spree",               2500,             100,              2500,
+  "Dresden",        "Elbe",                2500,             100,              2500, # memory error
+  "London",         "River Lea",           2500,             100,              2500,
+  "Bratislava",     "Danube",              2500,             100,              2500,
+  "Turin",          "Fiume Po",            2500,             100,              2500,
+  "Calarasi",       "Dunare",              2500,             100,              2500,
+  "Bristol",        "River Avon",          2500,             100,              2500,
+  "Dresden",        "Geberbach",           2500,             100,              2500,
+  "Rotterdam",      "Rotte",               2500,             100,              2500,
+  "Leiden",         "Rijn",                2500,             100,              2500,
+  "Delft",          "Schie",               2500,             100,              2500,
 
-# buffer_distance <- 3500 # for some cities
+)
+# nolint end
+
 
 get_delineate_layers <- function(
   city_name, river_name, network_buffer, buildings_buffer,
@@ -77,12 +89,18 @@ get_delineate_layers <- function(
 }
 
 plot_delineations <- function(x, setup) {
-  file_name <- paste0("delineation_", setup$city_name, "_", setup$river_name , ".png")
-  file_path <- file.path("figures", file_name)
+  file_name <- paste0(
+    "delineation_", setup$city_name, "_", setup$river_name, ".png"
+  )
+  file_path <- file.path("plots", file_name)
+
+  if (file.exists(file_path)) {
+    print(paste("Plot already exists:", file_path))
+    return()
+  }
 
   png(
-    filename = file_path, width = 8, height = 5, res = 600, units = "in",
-    bg = NA
+    filename = file_path, width = 8, height = 8, res = 600, units = "in"
   )
 
   plot(x$corridor)
@@ -98,17 +116,24 @@ plot_delineations <- function(x, setup) {
   title_text <- paste0(
     "Delineation of ", setup$river_name, " in ", setup$city_name
   )
-  title(main = title_text)
+  subtitle_text <- paste0(
+    "Network Buffer: ", setup$network_buffer, "m, ",
+    "Buildings Buffer: ", setup$buildings_buffer, "m, ",
+    "DEM Buffer: ", setup$dem_buffer, "m"
+  )
+  title(main = title_text, sub = subtitle_text, cex.main = 1.5, cex.sub = 0.5)
 
+  par(xpd = TRUE)
   legend(
     "topright",
     legend = c(
       "Corridor", "Valley", "River Surface", "River Centerline",
       "Streets", "Railways", "Segments", "City Boundary"
-      ),
+    ),
     col = c(
       "red", "gold", "lightblue",
-    "lightblue", "grey", "darkgrey", "blue", "black"
+      "lightblue", "grey", "darkgrey",
+      "blue", "black"
     ),
     lwd = c(2, 1, 1, 2, 0.5, 0.5, 1, 1),
     bty = "n", cex = 0.8
@@ -118,25 +143,33 @@ plot_delineations <- function(x, setup) {
   dev.off()
 }
 
-save_delineations <- function(x, setup) {
+save_delineations_layers <- function(x, setup) {
   # save all the layers to a geopackage file using sf::st_write
   file_name <- paste0(
     "delineation_", setup$city_name, "_", setup$river_name, ".gpkg"
   )
   file_path <- file.path("data", file_name)
+
+  # Check if the file already exists, do nothing
+  if (file.exists(file_path)) {
+    print(paste("File already exists:", file_path))
+    return()
+  }
+
   for (layer_name in names(x)) {
     sfc_layer <- x[[layer_name]]
     if (inherits(sfc_layer, "sfnetwork")) {
-      print(paste("converting network to sfc:", layer_name))
       nodes_sf <- sf::st_as_sf(sfc_layer, what = "nodes")
+      sf::st_write(
+        nodes_sf, file_path, layer = paste0(layer_name, "_nodes"), append = TRUE
+      )
+
       edges_sf <- sf::st_as_sf(sfc_layer, what = "edges")
-      sfc_layer <- rbind(nodes_sf, edges_sf)
-      sf::st_write(sfc_layer, file_path, layer = layer_name, append = TRUE)
-    } else if (inherits(sfc_layer, "SpatRaster")) {
-      print(paste("writing a SpatRaster:", layer_name))
-      terra::writeRaster(sfc_layer, file_path, overwrite = TRUE)
+      sf::st_write(
+        edges_sf, file_path, layer = paste0(layer_name, "_edges"), append = TRUE
+      )
+
     } else if (inherits(sfc_layer, "sfc")) {
-      print(paste("object is already sfc:", layer_name))
       sf::st_write(sfc_layer, file_path, layer = layer_name, append = TRUE)
     } else {
       print(paste("object is not sfc and not stored:", layer_name))
@@ -147,34 +180,59 @@ save_delineations <- function(x, setup) {
   # Connect to the GeoPackage again for custom attributes
   con <- RPostgreSQL::dbConnect(RSQLite::SQLite(), file_path)
   RPostgreSQL::dbWriteTable(
-    con, "delineation_attributes", setup, overwrite = TRUE
+    con, "delineation_metadata", setup, row.names = FALSE
   )
   RPostgreSQL::dbDisconnect(con)
-  print(paste("Saving attributes done!", file_path))
+  print(paste("Saving metadata done!", file_path))
+}
+
+save_dem <- function(dem, setup) {
+  file_name <- paste0("dem_", setup$city_name, "_", setup$river_name, ".tif")
+  file_path <- file.path("data", file_name)
+
+  # Check if the file already exists, do nothing
+  if (file.exists(file_path)) {
+    print(paste("File already exists:", file_path))
+    return()
+  }
+  terra::writeRaster(dem, file_path)
+  print(paste("Saving DEM done!", file_path))
 }
 
 # Delineate all corridors
-# delineations <- vector("list", length = nrow(city_river_names))
-# for (i in seq_along(delineations)) {
-#   setup <- city_river_names[i, ]
-#   print(paste("Processing", setup$city_name, setup$river_name))
-#   delineations[[i]] <- get_delineate_layers(
-#     setup$city_name,
-#     setup$river_name,
-#     setup$network_buffer,
-#     setup$buildings_buffer,
-#     setup$dem_buffer,
-#     force_download = FALSE
-#   )
-#   print(paste("Finished processing", setup$city_name, setup$river_name))
-# }
+# if delineations variable does not exist, create it
+if (!exists("delineations")) {
+  delineations <- vector("list", length = nrow(city_river_names))
+} else {
+  # if delineations variable exists, check if nrows match
+  if (length(delineations) != nrow(city_river_names)) {
+    stop(
+      "delineations variable exists, but city_river_names has different length!"
+    )
+  }
+}
 
-# # # Save each delineation into a single geopackage file
-# for (i in seq_along(delineations)) {
-#   save_delineations(delineations[[i]], city_river_names[i, ])
-# }
-
-# Plot each delineation
 for (i in seq_along(delineations)) {
+  setup <- city_river_names[i, ]
+
+  # if delineations[[i]] does not exist, get it
+  if (is.null(delineations[[i]])) {
+    print(paste("Processing", setup$city_name, setup$river_name))
+    delineations[[i]] <- get_delineate_layers(
+      setup$city_name,
+      setup$river_name,
+      setup$network_buffer,
+      setup$buildings_buffer,
+      setup$dem_buffer,
+      force_download = FALSE
+    )
+    print(paste("Finished processing", setup$city_name, setup$river_name))
+  }
+
+  # Save each delineation into a single geopackage file and dem to tiff
+  save_delineations_layers(delineations[[i]], city_river_names[i, ])
+  save_dem(delineations[[i]]$dem, city_river_names[i, ])
+
+  # Plot each delineation and save it as a png
   plot_delineations(delineations[[i]], city_river_names[i, ])
 }
